1a5ca9eea4ebe26025c722c18efb1c4432ebe2d7
thnaing1 <95155550+thnaing1@users.noreply.github.com>
2022-09-23 18:45:43 -0700

    Update SQLQuerytableau.sql

--- SQLQuerytableau.sql
+++ SQLQuerytableau.sql
@@ -23,17 +23,6 @@ where continent is not null
 --Group By date
 order by 1,2
 
--- Just a double check based off the data provided
--- numbers are extremely close so we will keep them - The Second includes "International"  Location
-
-
---Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
---From PortfolioProject..CovidDeaths
-----Where location like '%states%'
---where location = 'World'
-----Group By date
---order by 1,2
-
 
 -- 2. 
 
